1ad1d990776b73ea5583c654f4b4f880166805f0
don-bi <dbi30@stuy.edu>
2022-08-24 21:34:22 -0400

    added stuff to donation html

--- js/news.js
+++ js/news.js
@@ -1,29 +1,6 @@
-//for every new page in pages directory, just add a new dictionary to make the data
-//dont include .html for pagelink
-/*
-    {
-        title: "",
-        date: "",
-        caption: "",
-        pagelink: "",
-    },
-*/
-var cards = [
-    {
-        title: "First News!",
-        date: "January 08, 2005",
-        caption: "I don't know what to put as a caption",
-        pagelink: "01-08-2005",
-    },
-    {
-        title: "test",
-        date: "August 29, 199293",
-        caption: "lol",
-        pagelink: "08-29-199293",
-    },
-];
+import {cards} from "data.js";
 
-//fullcard is all the boxes combined
+//fullcard is resulting html of cards combined
 var fullcard = ``;
 
 //goes through cards variable and makes a new box for every dictionary
