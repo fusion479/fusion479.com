e383ead435ecf749641a00d9fc20d872c48925ce
don-bi <dbi30@stuy.edu>
2022-10-02 16:56:45 -0400

    added new bios

--- js/data.js
+++ js/data.js
@@ -31,15 +31,33 @@ export const memberList = [
         blurb: "I've always been a sort of a computer fanatic growing up, hardware and software are just things I find really cool. After joining 479 as a sophomore, I realized this was just the place to foster the skills I would need going forward while also doing something enjoyable with like-minded people. It's worth the time to spend the time working and learning with my teammates through thick and thin. I'm still learning the ropes and getting adjusted to being on the team now that school is in person, but with everyone else's support, I think I'll do just fine. Outside of robotics, my hobbies include playing video games, keeping up with tech, and music.",
     },
     {
-        photo: "",
+        photo: "shrek.jpg",
         name: "Daniel (Haokun) Xu",
         title: "Vice President",
         blurb: "Robotics and technology have fascinated me ever since I was a child. Joining 479 as a sophmore, I was a little late to the party, but I felt like I was still welcomed and encouraged to learn just like everyone else. The adrenaline you get watching the team succeed is unlike any other, and you feel kind of proud in a way. FTC has allowed me to develop my skills as an amateur engineer, machinist, software devloper, and team leader, and has made me into a more well-rounded individual. I am sure this is true for everyone else on the team and anyone else who decides to join in the future.",
     },
     {
-        photo: "",
+        photo: "shrek.jpg",
         name: "Kellen Yu",
         title: "Head of Engineering",
         blurb: "I have been passionate about engineering for as long as I can remember. I joined 479 as a freshman, and I can proudly say that I have no regrets in joining! I love how the general attitude of this team is to first try an idea out before judging it as there is not necessarily such a thing as a 'bad idea'. The open attitude of the team makes you feel that you are not only working together to build a robot and compete, but to also learn with one another and have fun. Apart from robotics, I like to make music, and mess around with electronics.",
+    },
+    {
+        photo: "shrek.jpg",
+        name: "Victoria Reguyal",
+        title: "",
+        blurb: "I’m a sophomore and I joined Team 479 last school year to learn about engineering and electronics while having fun with robots. Though I’ve been interested in STEM since elementary school, I started out with hardly any relevant experience. But in FTC, I have been a part of a warm community with people who push each other to learn and achieve more. Outside of school, I like to spend my time reading, playing games, and skateboarding.",
+    },
+    {
+        photo: "shrek.jpg",
+        name: "Khin Aung",
+        title: "",
+        blurb: "I'm a junior that joined 479 last year, because I was looking for something to do and decided to join this team because @Daniel  talked about joining it and it sounded interesting. I've always been a bit of a technology nerd, but I wanted to get into the nitty gritty of things and I figured this would be a good place to start. Everyone here is very cool and friendly! (And what Victoria said because I can't phrase it better 🤣 ). Outside of school I like to listen to jpop and edm, while also playing rhythm games like osu! when I get the time.",
+    },
+    {
+        photo: "shrek.jpg",
+        name: "William Guo",
+        title: "",
+        blurb: "From learning about CAD in afterschool programs to messing around with the lego robotics,  I've always enjoyed technology and robots since middle school. I decided to join the robotic team my freshman year to push my knowledge even further. Being surrounded by such supportive peers really helped me build the support for my foundation while also pushing me to strive for greater heights. Outside of school, I like playing fps games, racket sports, basketball and sleeping.",
     },
 ]
