efe0bb67f550bbd3acfbca83ccb002aa47ca3a87
don-bi <dbi30@stuy.edu>
2022-10-16 23:18:06 -0400

    finished home layout gonna add new text later

--- js/news.js
+++ js/news.js
@@ -17,11 +17,35 @@ newsCards.forEach (news => {
 })
 
 //console.log(fullCard);
-
 class NewsTemplate extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `${fullCard}`
     }
 }
 
+var indexNews = ``;
+
+for (var i = 1; i < 4; i ++) {
+    const currentNewsCard = newsCards[newsCards.length-i];
+    var border = ` border`;
+    if (i == 3) border = ``;
+    if (currentNewsCard != null) {
+        var data = Object.values(currentNewsCard);
+        indexNews += `
+        <a href="pages/${data[3]}.html" class="news-box${border}">
+            <div class="news-title">${data[0]}</div>
+            <div class="news-creation">Created on ${data[1]}</div>
+            <div class="news-caption">${data[2]}</div>
+        </a>
+        `
+    }
+}
+
+class IndexNewsTemplate extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `${indexNews}`
+    }
+}
+
 customElements.define("news-cards",NewsTemplate)
+customElements.define("index-news",IndexNewsTemplate)
