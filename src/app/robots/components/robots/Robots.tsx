d7ff8c6be12db9067ee42ad2cc09ab8cc884eb25
Bi1ku <2008owenshi@gmail.com>
2023-12-16 23:18:29 -0500

    stuff

--- src/app/robots/components/robots/Robots.tsx
+++ src/app/robots/components/robots/Robots.tsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import Info from "./Info";
 import Timeline from "./Timeline";
+import Name from "./Name";
 
 export default function Robots() {
   const [active, setActive] = useState(0);
 
   return (
-    <div className="flex p-10 justify-between">
-      <Timeline setActive={setActive} active={active} />
-      <Info active={active} />
-    </div>
+    <>
+      <Name />
+      <div className="flex p-10 justify-between">
+        <Timeline setActive={setActive} active={active} />
+        <Info active={active} />
+      </div>
+    </>
   );
 }
