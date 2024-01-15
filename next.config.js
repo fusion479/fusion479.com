47161e72072f09165fac05dfe0cdc40156985ca3
Bi1ku <2008owenshi@gmail.com>
2024-01-15 15:27:45 -0500

    test

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
+  output: "standalone",
 };
 
 module.exports = nextConfig;
