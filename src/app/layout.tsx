4141955501cc1d4a8e968cf7db23d8a1825c429e
Owen <2008owenshi@gmail.com>
2024-01-15 17:46:47 -0500

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+      </Head>
       <body className={inter.className}>
         <Navbar />
         {children}
