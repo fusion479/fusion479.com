dca3e0e7d603766c7fce832851eb46b16f46f65b
Owen <2008owenshi@gmail.com>
2024-01-15 17:52:35 -0500

    Revert "favicon"
    This reverts commit 4141955501cc1d4a8e968cf7db23d8a1825c429e.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +18,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
-      </Head>
       <body className={inter.className}>
         <Navbar />
         {children}
