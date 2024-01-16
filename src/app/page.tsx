39f863e4e601a80defd78f3d479e85802fbf81d3
Owen <2008owenshi@gmail.com>
2024-01-15 21:45:58 -0500

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,10 @@ import About from "./components/About";
 import Game from "./components/Game";
 import Sponsors from "./components/Sponsors";
 import Background from "@/components/Background";
-import Head from "next/head";
 
 export default function Landing() {
   return (
     <Background>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Container className="relative">
         <Hero />
         <Sponsors />
