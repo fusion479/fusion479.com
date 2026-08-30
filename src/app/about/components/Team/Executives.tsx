c3e4ca91f095e2d96b1d01004010aa3d0283c454
Cassandra Chan <cassandrachan0137@gmail.com>
2026-08-30 12:10:55 -0400

    update execs

--- src/app/about/components/Team/Executives.tsx
+++ src/app/about/components/Team/Executives.tsx
@@ -1,25 +1,24 @@
 import Image from "next/image";
-import jason from "../../../../../public/team/jason.jpg";
-import owen from "../../../../../public/team/owen.jpg";
+import avatar from "../../../../../public/team/avatar.webp";
 
 export default function Executives() {
   return (
     <div className="gap-10 grid auto-rows-fr grid-cols-1 sm:grid-cols-2">
       {new Array(
         {
-          name: "Jason Zhao",
-          role: "Head of Engineering",
-          avatar: jason,
+          name: "Cassandra Chan",
+          role: "President",
+          avatar: avatar,
           description:
-            "In my sophomore year, I joined the FTC robotics team 479, feeling a mix of excitement and nerves. The world of robotics and engineering was new to me, and I was unsure of how I would fit in. However, as I spent more time with the team, I quickly became immersed in the technical challenges and collaborative environment. I developed a passion for engineering, learning new skills and concepts along the way. The support from my teammates helped me grow more confident, and I found myself contributing actively to the team's projects, gaining valuable experience in the process.",
+            "",
         },
 
         {
-          name: "Owen Shi",
+          name: "Aidan Gray",
           role: "Vice President",
-          avatar: owen,
+          avatar: avatar,
           description:
-            "I joined Stuy Fusion a little late, as a sophomore, unsure of where I could contribute. Determined to help out, I quickly immersed myself in learning FTC software and discovered my passion for programming and problem-solving. Being part of Stuy Fusion has sharpened my technical skills and taught me the value of collaboration and perseverance. Outside of robotics, I enjoy playing video games and swimming, both of which help me unwind and stay motivated in everything I do.",
+            "",
         },
       ).map((v, i) => (
         <div
