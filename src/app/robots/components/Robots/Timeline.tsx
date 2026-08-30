c418a70c7210a1dabb74febecb28708ee39bf307
Cassandra Chan <cassandrachan0137@gmail.com>
2026-08-29 20:04:45 -0400

    Update Timeline.tsx

--- src/app/robots/components/Robots/Timeline.tsx
+++ src/app/robots/components/Robots/Timeline.tsx
@@ -23,7 +23,7 @@ export default function Timeline({
             className={`inline-block tracking-widest rounded-lg font-light text-xs mr-2 text-gray-500
               ${i === active && "text-blue-700"}`}
           >
-            {2024 - i}:
+            {2025 - i}:
           </span>
           <button
             className={`relative block px-4 py-2 my-4 rounded-lg hover:bg-blue-100 hover:-translate-y-0.5 hover:text-blue-600 duration-150 ${i === active ? "bg-blue-50 text-blue-600" : "text-slate-700"} ${!v.description && "cursor-not-allowed"}`}
